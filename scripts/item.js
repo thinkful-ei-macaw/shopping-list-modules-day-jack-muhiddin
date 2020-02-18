3f0c5d9e372d76e5589579d20197711596d69985
Jack Lansing <jack.lansing@live.com>
2020-02-18 14:24:16 -0500

    completed third exercise

--- scripts/item.js
+++ scripts/item.js
@@ -1,5 +1,17 @@
 
+function validateName(name) {
+  if(!name) {
+    throw new TypeError('Name must not be blank');
+  }
+}
 
+function create(name) {
+  return {
+    id: cuid(),
+    name: name,
+    checked: false
+  };
+}
 
-export default {};
+export default { validateName, create };
 
