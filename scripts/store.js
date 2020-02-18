7fea9eff0d536bcbe16d8bf29274de5dc34bad32
Jack Lansing <jack.lansing@live.com>
2020-02-18 14:47:09 -0500

    completed ex5

--- scripts/store.js
+++ scripts/store.js
@@ -3,8 +3,48 @@ import item from './item.js';
 const items = [];
 let hideCheckedItems = false;
 
+function findByID(id) {
+  return items.find(item => item.id === id);
+}
+
+function addItem(name) {
+  try {
+    item.validateName(name);
+    const newItem = item.create(name);
+    this.items.push(newItem);
+  } catch(error) {
+    console.log(`Cannot add item: ${error.message}`);
+  }
+}
+
+function findAndToggleChecked(id) {
+  const currentItem = this.findByID(id);
+  currentItem.checked = !currentItem.checked;
+}
+
+function findAndUpdateName(id, newName) {
+  try {
+    item.validateName(newName);
+    const currentItem = this.findByID(id);
+    currentItem.name = newName;
+  } catch(error) {
+    console.log(`Cannot update name: ${error.message}`);
+  }
+}
+
+function findAndDelete(id) {
+  const currentItem = this.findByID(id);
+  const itemIndex = this.items.indexOf(currentItem);
+  this.items.splice(itemIndex, 1);
+}
+
 export default {
   items,
-  hideCheckedItems
+  hideCheckedItems,
+  findByID,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete
 };
 
